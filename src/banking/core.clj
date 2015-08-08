480da15ce48981e4e52cb24bd1d17232af81004a
derwolfe <chriswwolfe@gmail.com>
2015-08-08 14:06:47 -0500

    dropping fixtures, tests are doing a bit better

--- src/banking/core.clj
+++ src/banking/core.clj
@@ -1,7 +1,10 @@
 (ns banking.core)
 
-(defn make-account [acct-id]
-  (ref {:id acct-id :money 0}))
+(defn seed-account [id seed]
+  {:id id :money seed})
+
+(defn make-account! [acct-id seed]
+  (ref {:id acct-id :money seed}))
 
 (defn account-id [account]
   (:id @account))
@@ -9,23 +12,28 @@
 (defn balance [account]
   (:money @account))
 
-(defn credit [account amount]
-  (dosync
-   (alter account
-          (fn [a m] (assoc a :money (+ (:money a) amount))) amount)))
+(defn- credit [account amount]
+  (assoc account :money (+ (:money account) amount)))
 
-(defn debit [account amount]
-  (dosync
-   (when (> amount (balance account))
-     (throw (Exception. "Insufficient Funds")))
-     (credit account (- amount))))
+(defn- debit [account amount]
+  (if (> amount (:money account))
+    (throw (Exception. "Insufficient Funds"))
+    (credit account (- amount))))
 
-(defn transfer [from to amount]
+(defn credit! [account amount]
   (dosync
-   (when (>= (:money from) amount)
+   (alter account credit amount)))
+
+(defn debit! [account amount]
+  (dosync
+   (alter account debit amount)))
+
+(defn transfer! [from to amount]
+  (dosync
+   (when (>= (balance from) amount)
      (Thread/sleep 100)
-     (debit from amount)
-     (credit to amount))))
+     (debit! from amount)
+     (credit! to amount))))
 
 (defn account-balances [accts]
   (map
--- test/banking/core_test.clj
+++ test/banking/core_test.clj
@@ -2,61 +2,63 @@
   (:require [clojure.test :refer :all]
             [banking.core :refer :all]))
 
-(def checking (make-account 1))
-(def savings (make-account 2))
-
-(defn my-fixture [f]
-  (dosync
-    (ref-set checking {:id 1 :money 100})
-    (ref-set savings {:id 2 :money 100})
-  (f)))
-
-(use-fixtures :each my-fixture)
-
 (deftest test-balance
-  (is (= 100 (balance checking))))
+  (let [c (make-account! 1 100)]
+    (is (= 100 (balance c)))))
 
 (deftest test-credit
-  (credit checking 60)
-  (is (= 160 (balance checking))))
+  (let [c (make-account! 1 100)]
+    (credit! c 60)
+    (is (= 160 (balance c)))))
 
 (deftest test-debit
-  (debit checking 100)
-  (is (= 0 (balance checking))))
+  (let [c (make-account! 1 100)]
+    (debit! c 100)
+    (is (= 0 (balance c)))))
 
 (deftest test-overdraw-exception
-  (is (thrown? Exception
-        (debit checking 101))))
+  (let [c (make-account! 1 100)]
+    (is (thrown? Exception
+                 (debit! c 101)))))
 
-;; (deftest test-concurrent-credits-debits
-;;   (doall (pmap #(do
-;;                   (credit checking (+ % 1))
-;;                   (debit checking %))
-;;                (take 2 (repeat 5))))
-;;   (is (= 200 (balance checking))))
+(deftest test-concurrent-credits-debits
+  (let [a (make-account! 1 100)]
+    (doall
+     (pmap
+      #(do
+         (credit! a (+ % 1))
+         (debit! a %))
+      (take 100 (repeatedly #(rand-int 25)))))
+    (is (= 200 (balance a)))))
 
 (deftest test-transfer
-  (transfer savings checking 25)
-  (is (= 75 (balance savings)))
-  (is (= 125  (balance checking))))
+  (let [a (make-account! 1 100)
+        b (make-account! 2 100)]
+    (do
+      (transfer! b a 25)
+      (is (= 75 (balance b)))
+      (is (= 125  (balance a))))))
 
 (deftest test-no-transfer-if-overdrawn
-  (transfer checking savings 101)
-  (is (= 100 (balance savings)))
-  (is (= 100 (balance checking))))
+  (let [a (make-account! 1 1)
+        b (make-account! 2 1)]
+    (transfer! a b 101)
+    (is (= 1 (balance a)))
+    (is (= 1 (balance b)))))
 
-;; this is never terminating!
-;; (deftest test-concurrent-transfers
-;;   "do one hundred transfers in and out of each account using the same amount."
-;;   (doall (pmap #(do
-;;            (transfer checking savings %)
-;;            (transfer savings checking %))
-;;         ;; make a lazy sequence of 100 transactions to perform
-;;         (take 100 (repeatedly #(rand-int 25)))))
-;;   (is (= 200 (+ (balance savings) (balance checking)))))
+(deftest test-concurrent-transfers
+  (let [a (make-account! 1 100)
+        b (make-account! 2 100)]
+    (doall
+     (pmap
+      #(do
+         (transfer! a b %)
+         (transfer! b a %))
+      (take 100 (repeatedly #(rand-int 25)))))
+    (is (= 100 (balance a)))
+    (is (= 100 (balance b)))))
 
 (deftest test-prints-balances
-  (let [act1 (make-account "act1")]
-    (credit act1 100)
+  (let [act1 (make-account! "act1" 100)]
     (is (= '("act1 has 100")
            (account-balances [act1])))))
